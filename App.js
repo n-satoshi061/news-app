ed3e905a55b56d4911996ef9a38b0bb53061d179
n-satoshi061 <n.satoshi061@gmail.com>
2020-09-29 17:06:13 +0900

    UIの作成

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +9,50 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row'
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'space-between'
+  },
+  text: {
+    fontSize: 16
+  },
+  subText: {
+    fontSize: 12,
+     color: 'gray'
+  }
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View 
+        style={styles.itemContainer}>
+          <View style={styles.leftContainer}>
+            <Image
+              style={{width: 100, height: 100}}
+              source={{
+                uri: 'https://picsum.photos/200/300',
+              }}
+            />
+          </View>
+          <View style={styles.rightContainer}>
+            <Text numberOfLines={3} style={styles.text}>
+            ImageResizeMode is an Enum for different image resizing modes, set via the resizeMode style property on Image components. The values are contain, cover, stretch, center, repeat.
+            </Text>
+            <Text style={styles.subText}>React News</Text>
+          </View>
+        </View>
+    </View>
+  );
+}
