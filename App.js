ed52c6674378552755cc61d19f47acc77c8a3c35
n-satoshi061 <n.satoshi061@gmail.com>
2020-09-29 18:45:05 +0900

    コンポーネントの作成

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import ListItem from './components/ListItem';
+import articles from './dummies/articles';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   itemContainer: {
     height: 100,
     width: '100%',
     borderColor: 'gray',
     borderWidth: 1,
-    flexDirection: 'row'
+    flexDirection: 'row',
   },
   leftContainer: {
     width: 100,
@@ -22,37 +21,31 @@ const styles = StyleSheet.create({
   rightContainer: {
     flex: 1,
     padding: 10,
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   text: {
-    fontSize: 16
+    fontSize: 16,
   },
   subText: {
     fontSize: 12,
-     color: 'gray'
-  }
+    color: 'gray',
+  },
 });
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View 
-        style={styles.itemContainer}>
-          <View style={styles.leftContainer}>
-            <Image
-              style={{width: 100, height: 100}}
-              source={{
-                uri: 'https://picsum.photos/200/300',
-              }}
-            />
-          </View>
-          <View style={styles.rightContainer}>
-            <Text numberOfLines={3} style={styles.text}>
-            ImageResizeMode is an Enum for different image resizing modes, set via the resizeMode style property on Image components. The values are contain, cover, stretch, center, repeat.
-            </Text>
-            <Text style={styles.subText}>React News</Text>
-          </View>
-        </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
   );
 }
